a75228dad3e190b4c3b3040fff1543e9be9b042a
tonygiang <tony_s_giang@yahoo.com>
2022-07-27 14:27:29 +0700

    Project push.

---
+++ IDictionary.GetOrAdd.cs
@@ -0,0 +1,117 @@
+﻿// A part of the C# Language Syntactic Sugar suite.
+
+using System.Collections.Generic;
+using System;
+
+namespace CLSS
+{
+  public static partial class IDictionaryGetOrAdd
+  {
+    /// <summary>
+    /// Adds a key/value pair to the <see cref="IDictionary{TKey, TValue}"/> if
+    /// the key does not already exist. Returns the new value, or the existing
+    /// value if the key exists.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the keys in
+    /// <paramref name="source"/>.</typeparam>
+    /// <typeparam name="TValue">The type of the values in
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="key">The key of the element to add.</param>
+    /// <param name="value">The value to be added, if the key does not already
+    /// exist.</param>
+    /// <returns>The value for the key. This will be either the existing value
+    /// for the key if the key is already in the dictionary, or the new value if
+    /// the key was not in the dictionary.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <paramref name="key"/> is null.</exception>
+    public static TValue GetOrAdd<TKey, TValue>(
+      this IDictionary<TKey, TValue> source,
+      TKey key,
+      TValue value)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (key == null) throw new ArgumentNullException("key");
+
+      TValue resultingValue = value;
+      if (!source.TryGetValue(key, out resultingValue)) source[key] = value;
+      return resultingValue;
+    }
+
+    /// <summary>
+    /// Adds a key/value pair to the <see cref="IDictionary{TKey, TValue}"/> by
+    /// using the specified function if the key does not already exist. Returns
+    /// the new value, or the existing value if the key exists.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the keys in
+    /// <paramref name="source"/>.</typeparam>
+    /// <typeparam name="TValue">The type of the values in
+    /// <paramref name="source"/>.</typeparam>
+    /// <param name="key">The key of the element to add.</param>
+    /// <param name="valueFactory">The function used to generate a value for the
+    /// key.</param>
+    /// <returns>The value for the key. This will be either the existing value
+    /// for the key if the key is already in the dictionary, or the new value if
+    /// the key was not in the dictionary.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/>,
+    /// <paramref name="key"/> or <paramref name="valueFactory"/> is null.
+    /// </exception>
+    public static TValue GetOrAdd<TKey, TValue>(
+      this IDictionary<TKey, TValue> source,
+      TKey key,
+      Func<TKey, TValue> valueFactory)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (key == null) throw new ArgumentNullException("key");
+      if (valueFactory == null) throw new ArgumentNullException("valueFactory");
+
+      TValue resultingValue;
+      if (!source.TryGetValue(key, out resultingValue))
+      {
+        resultingValue = valueFactory(key);
+        source[key] = resultingValue;
+      }
+      return resultingValue;
+    }
+
+    /// <summary>
+    /// Adds a key/value pair to the <see cref="IDictionary{TKey, TValue}"/> by
+    /// using the specified function if the key does not already exist. Returns
+    /// the new value, or the existing value if the key exists.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the keys in
+    /// <paramref name="source"/>.</typeparam>
+    /// <typeparam name="TValue">The type of the values in
+    /// <paramref name="source"/>.</typeparam>
+    /// <typeparam name="TArg">The type of an argument to pass into
+    /// <paramref name="valueFactory"/>.</typeparam>
+    /// <param name="key">The key of the element to add.</param>
+    /// <param name="valueFactory">The function used to generate a value for the
+    /// key.</param>
+    /// <param name="factoryArgument">An argument value to pass into
+    /// <paramref name="valueFactory"/>.</param>
+    /// <returns>The value for the key. This will be either the existing value
+    /// for the key if the key is already in the dictionary, or the new value if
+    /// the key was not in the dictionary.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="source"/>,
+    /// <paramref name="key"/> or <paramref name="valueFactory"/> is null.
+    /// </exception>
+    public static TValue GetOrAdd<TKey, TValue, TArg>(
+      this IDictionary<TKey, TValue> source,
+      TKey key,
+      Func<TKey, TArg, TValue> valueFactory,
+      TArg factoryArgument)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (key == null) throw new ArgumentNullException("key");
+      if (valueFactory == null) throw new ArgumentNullException("valueFactory");
+
+      TValue resultingValue;
+      if (!source.TryGetValue(key, out resultingValue))
+      {
+        resultingValue = valueFactory(key, factoryArgument);
+        source[key] = resultingValue;
+      }
+      return resultingValue;
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+﻿# CLSS.ExtensionMethods.IDictionary.GetOrAdd
+
+### Problem
+
+[`GetOrAdd`](https://docs.microsoft.com/en-us/dotnet/api/system.collections.concurrent.concurrentdictionary-2.getoradd) is a convenient method for `ConcurrentDictionary` but not for other `IDictionary` types in the standard class library. This method is often used to cache results of pure functions, ie functions that always yield a deterministic result for each argument.
+
+### Solution
+
+This package provides `GetOrAdd` method to all `IDictionary` types.
+
+Example:
+
+```
+using CLSS;
+
+var NumberSpellings = new Dictionary<int, string>();
+Func<int, string> NumberToSpelling = n => { /* Implementation */ };
+var spelling_285 = NumberSpellings.GetOrAdd(285, NumberToSpelling);
+```
+
+##### This package is a part of the [C# Language Syntactic Sugar suite](https://github.com/tonygiang/CLSS).
