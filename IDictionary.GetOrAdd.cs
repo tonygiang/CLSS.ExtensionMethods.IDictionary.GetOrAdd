22da03496402f3234ae8e78a5274d90a2a6004eb
tonygiang <tony_s_giang@yahoo.com>
2022-08-04 18:29:51 +0700

    Version 1.1
    - Reduce comment redundancy.

--- IDictionary.GetOrAdd.cs
+++ IDictionary.GetOrAdd.cs
@@ -12,104 +12,85 @@ namespace CLSS
     /// the key does not already exist. Returns the new value, or the existing
     /// value if the key exists.
     /// </summary>
-    /// <typeparam name="TKey">The type of the keys in
-    /// <paramref name="source"/>.</typeparam>
-    /// <typeparam name="TValue">The type of the values in
-    /// <paramref name="source"/>.</typeparam>
-    /// <param name="key">The key of the element to add.</param>
-    /// <param name="value">The value to be added, if the key does not already
-    /// exist.</param>
+    /// <typeparam name="TKey"><inheritdoc cref="IDictionary{TKey, TValue}"
+    /// path="/typeparam[@name='TKey']"/></typeparam>
+    /// <typeparam name="TValue"><inheritdoc cref="IDictionary{TKey, TValue}"
+    /// path="/typeparam[@name='TValue']"/></typeparam>
+    /// <param name="dictionary">A dictionary with keys of type
+    /// <typeparamref name="TKey"/> and values of type
+    /// <typeparamref name="TValue"/>.</param>
+    /// <param name="key">
+    /// <inheritdoc cref="IDictionary{TKey, TValue}.Add(TKey, TValue)"
+    /// path="/param[@name='key']"/></param>
+    /// <param name="value">
+    /// <inheritdoc cref="IDictionary{TKey, TValue}.Add(TKey, TValue)"
+    /// path="/param[@name='value']"/></param>
     /// <returns>The value for the key. This will be either the existing value
     /// for the key if the key is already in the dictionary, or the new value if
     /// the key was not in the dictionary.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="source"/> or
+    /// <exception cref="ArgumentNullException"><paramref name="dictionary"/> or
     /// <paramref name="key"/> is null.</exception>
     public static TValue GetOrAdd<TKey, TValue>(
-      this IDictionary<TKey, TValue> source,
+      this IDictionary<TKey, TValue> dictionary,
       TKey key,
       TValue value)
     {
-      if (source == null) throw new ArgumentNullException("source");
+      if (dictionary == null) throw new ArgumentNullException("dictionary");
       if (key == null) throw new ArgumentNullException("key");
 
       TValue resultingValue = value;
-      if (!source.TryGetValue(key, out resultingValue)) source[key] = value;
+      if (!dictionary.TryGetValue(key, out resultingValue))
+        dictionary[key] = value;
       return resultingValue;
     }
 
+    /// <inheritdoc cref="GetOrAdd{TKey, TValue}(IDictionary{TKey, TValue}, TKey, TValue)"/>
     /// <summary>
     /// Adds a key/value pair to the <see cref="IDictionary{TKey, TValue}"/> by
     /// using the specified function if the key does not already exist. Returns
     /// the new value, or the existing value if the key exists.
     /// </summary>
-    /// <typeparam name="TKey">The type of the keys in
-    /// <paramref name="source"/>.</typeparam>
-    /// <typeparam name="TValue">The type of the values in
-    /// <paramref name="source"/>.</typeparam>
-    /// <param name="key">The key of the element to add.</param>
     /// <param name="valueFactory">The function used to generate a value for the
     /// key.</param>
-    /// <returns>The value for the key. This will be either the existing value
-    /// for the key if the key is already in the dictionary, or the new value if
-    /// the key was not in the dictionary.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="source"/>,
+    /// <exception cref="ArgumentNullException"><paramref name="dictionary"/>,
     /// <paramref name="key"/> or <paramref name="valueFactory"/> is null.
     /// </exception>
     public static TValue GetOrAdd<TKey, TValue>(
-      this IDictionary<TKey, TValue> source,
+      this IDictionary<TKey, TValue> dictionary,
       TKey key,
       Func<TKey, TValue> valueFactory)
     {
-      if (source == null) throw new ArgumentNullException("source");
+      if (dictionary == null) throw new ArgumentNullException("dictionary");
       if (key == null) throw new ArgumentNullException("key");
       if (valueFactory == null) throw new ArgumentNullException("valueFactory");
 
       TValue resultingValue;
-      if (!source.TryGetValue(key, out resultingValue))
+      if (!dictionary.TryGetValue(key, out resultingValue))
       {
         resultingValue = valueFactory(key);
-        source[key] = resultingValue;
+        dictionary[key] = resultingValue;
       }
       return resultingValue;
     }
 
-    /// <summary>
-    /// Adds a key/value pair to the <see cref="IDictionary{TKey, TValue}"/> by
-    /// using the specified function if the key does not already exist. Returns
-    /// the new value, or the existing value if the key exists.
-    /// </summary>
-    /// <typeparam name="TKey">The type of the keys in
-    /// <paramref name="source"/>.</typeparam>
-    /// <typeparam name="TValue">The type of the values in
-    /// <paramref name="source"/>.</typeparam>
-    /// <typeparam name="TArg">The type of an argument to pass into
-    /// <paramref name="valueFactory"/>.</typeparam>
-    /// <param name="key">The key of the element to add.</param>
-    /// <param name="valueFactory">The function used to generate a value for the
-    /// key.</param>
+    /// <inheritdoc cref="GetOrAdd{TKey, TValue}(IDictionary{TKey, TValue}, TKey, Func{TKey, TValue})"/>
     /// <param name="factoryArgument">An argument value to pass into
     /// <paramref name="valueFactory"/>.</param>
-    /// <returns>The value for the key. This will be either the existing value
-    /// for the key if the key is already in the dictionary, or the new value if
-    /// the key was not in the dictionary.</returns>
-    /// <exception cref="ArgumentNullException"><paramref name="source"/>,
-    /// <paramref name="key"/> or <paramref name="valueFactory"/> is null.
-    /// </exception>
     public static TValue GetOrAdd<TKey, TValue, TArg>(
-      this IDictionary<TKey, TValue> source,
+      this IDictionary<TKey, TValue> dictionary,
       TKey key,
       Func<TKey, TArg, TValue> valueFactory,
       TArg factoryArgument)
     {
-      if (source == null) throw new ArgumentNullException("source");
+      if (dictionary == null) throw new ArgumentNullException("dictionary");
       if (key == null) throw new ArgumentNullException("key");
       if (valueFactory == null) throw new ArgumentNullException("valueFactory");
 
       TValue resultingValue;
-      if (!source.TryGetValue(key, out resultingValue))
+      if (!dictionary.TryGetValue(key, out resultingValue))
       {
         resultingValue = valueFactory(key, factoryArgument);
-        source[key] = resultingValue;
+        dictionary[key] = resultingValue;
       }
       return resultingValue;
     }
